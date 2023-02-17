ca2691d3817f0fec1a7a071fe6e3729b2a933164
Bodo Kaiser <bodo.kaiser@physik.uni-muenchen.de>
2023-02-17 11:41:09 +0100

    added abstract Superposition and composite Solenoid types

--- src/mfd.jl
+++ src/mfd.jl
@@ -4,17 +4,23 @@ using PhysicalConstants.CODATA2018: μ_0
 
 μ₀ = ustrip(u"N/A^2", μ_0)
 
+export mfd
+
+# References
+# [1] Simpson, James C. et al. “Simple Analytic Expressions for the Magnetic Field of a Circular Current Loop.” (2001).
+# [2] Jang, Taehun, et al. "Off-axis magnetic fields of a circular loop and a solenoid for the electromagnetic induction of a magnetic pendulum." Journal of Physics Communications 5.6 (2021)
+
 """
     mfd(cl::CurrentLoop, ρ::Float64, z::Float64)
 
-The vectorial magnetic flux density due to a current loop in cylindrical coordinates with the current loop centered around the origin.
+The vectorial magnetic flux density due to a current loop in cylindrical coordinates with the current loop centered around the origin according to Ref. [1,2].
 """
 function mfd(cl::CurrentLoop, ρ::Float64, z::Float64)
     I = cl.current
     R = cl.radius
 
-    α² = R^2 + ρ^2 + z^2 - 2 * R * ρ
-    β = √(R^2 + ρ^2 + z^2 + 2 * R * ρ)
+    α² = R^2 + ρ^2 + z^2 - 2R * ρ
+    β = √(R^2 + ρ^2 + z^2 + 2R * ρ)
 
     k² = 1 - α² / β^2
 
--- src/types.jl
+++ src/types.jl
@@ -18,3 +18,34 @@ struct CurrentLoop <: Coil
     current::Float64
     radius::Float64
 end
+
+"""
+    Superposition(coils::Vector{<:Coil})
+
+A superposition of coils.
+"""
+abstract type Superposition <: Coil end
+
+"""
+    Solenoid(current, inner_radius, axial_turns, axial_spacing, radial_turns, radial_spacing)
+
+A solenoid with a given current, inner radius, axial turns, axial spacing, radial turns, and radial spacing.
+
+# Arguments
+- `current::Float64`: The current in the solenoid in Amps.
+- `inner_radius::Float64`: The inner radius of the solenoid in meters.
+- `axial_turns::Int`: The number of axial turns in the solenoid.
+- `axial_spacing::Float64`: The axial spacing between turns in meters.
+- `radial_turns::Int`: The number of radial turns in the solenoid.
+- `radial_spacing::Float64`: The radial spacing between turns in meters.
+"""
+struct Solenoid <: Superposition
+    current::Float64
+    inner_radius::Float64
+    axial_turns::Int
+    axial_spacing::Float64
+    radial_turns::Int
+    radial_spacing::Float64
+end
+
+end
